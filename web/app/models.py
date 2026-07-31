9fe04349e3f89a5b6d8ac41c454c6e8d2d439b48
Mohamed <mohamedelbergui356@gmail.com>
2026-07-31 20:37:15 +0100

    add Sevice and CheckResult models

--- web/app/models.py
+++ web/app/models.py
@@ -1,6 +1,6 @@
 from app.extensions import db
 from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
-from sqlalchemy import func, ForeignKey, String, Integer, Boolean, FLOAT, DATETIME
+from sqlalchemy import func, ForeignKey, String, Integer, Boolean, FLOAT, DateTime
 from datetime import datetime
 
 class Base(db.Model):
@@ -15,4 +15,24 @@ class User(Base):
     l_name: Mapped[str] = mapped_column(String(100))
     username: Mapped[str] = mapped_column(String(120),unique=True, nullable=False)
     password: Mapped[str] = mapped_column(String(255), nullable=False)
+
+class Service(Base):
+    __tablename__ = "services"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    url: Mapped[str] = mapped_column(String(300))
+    name: Mapped[str] = mapped_column(String(200))
+    check_interval_sec: Mapped[int] = mapped_column(Integer)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+
+class CheckResult(Base):
+    __tablename__ = "check_results"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    id_service: Mapped[int] = mapped_column(ForeignKey("services.id"))
+    status_code: Mapped[int] = mapped_column(Integer)
+    response_time_ms: Mapped[float] = mapped_column(FLOAT)
+    message: Mapped[str] = mapped_column(String(600))
+    timestamp: Mapped[datetime] = mapped_column(DateTime(timezone=True))
 
